d7f95e617e52f8374980bade47d9ced8c38add5e
LanLan77 <775172380@qq.com>
2023-08-29 11:56:28 +0930

    容器中找数据并删除的技巧

--- javasepro/api-app/src/com/alan/arraylist/ArrayListCase1.java
+++ javasepro/api-app/src/com/alan/arraylist/ArrayListCase1.java
@@ -1,4 +1,33 @@
 package com.alan.arraylist;
 
+import java.util.ArrayList;
+
 public class ArrayListCase1 {
+    // 目标：掌握从容器中找数据并删除的技巧
+
+    public static void main(String[] args) {
+        // 1. 创建一个ArrayList对象
+        ArrayList<String> arrayList = new ArrayList<String>();
+        arrayList.add("Java入门");
+        arrayList.add("枸杞");
+        arrayList.add("人字拖");
+        arrayList.add("特级枸杞");
+        arrayList.add("黑枸杞");
+        arrayList.add("枸杞子 ");
+        System.out.println(arrayList);
+
+        // 2. 从集合中找出包含枸杞的数据并且删除
+        for (int i = arrayList.size()-1; i>= 0 ; i--) {
+            // 3. 取出当前遍历到的数据
+            String ele = arrayList.get(i);
+            // 4. 判断这个数据是否包含枸杞
+            if(ele.contains("枸杞")){
+                arrayList.remove(ele);
+            }
+
+        }
+
+        System.out.println(arrayList);
+
+    }
 }
