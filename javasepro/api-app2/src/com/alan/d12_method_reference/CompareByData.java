44bd4f0255ea2370f9c5e2729cec955b6a802c47
LanLan77 <775172380@qq.com>
2023-11-28 22:32:53 +1030

    JDK8新特性：静态方法引用

--- javasepro/api-app2/src/com/alan/d12_method_reference/CompareByData.java
+++ javasepro/api-app2/src/com/alan/d12_method_reference/CompareByData.java
@@ -3,7 +3,7 @@ package com.alan.d12_method_reference;
 import com.alan.d10_arrays.Student;
 
 public class CompareByData {
-    public static int compareByAge(Student o1, Student o2){
-        return o1.getAge()-o2.getAge();
+    public static int compareByAge(Student o1, Student o2) {
+        return o1.getAge() - o2.getAge();
     }
 }
