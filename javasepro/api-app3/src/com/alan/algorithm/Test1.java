e5cffbb4b9df9c4b4a705741c8781a4e409d2f50
LanLan77 <775172380@qq.com>
2023-11-30 14:01:44 +1030

    算法：选择排序

---
+++ javasepro/api-app3/src/com/alan/algorithm/Test1.java
@@ -0,0 +1,23 @@
+package com.alan.algorithm;
+
+import java.util.Arrays;
+
+// 目标：掌握冒泡排序的编写
+public class Test1 {
+    public static void main(String[] args) {
+        //1. 准备一个数组
+        int[] arr = {5,2,3,1,4,6};
+        int max = arr[0];
+        for (int i = 0; i < arr.length-1; i++) {
+            for (int j = 0; j < arr.length-i-1; j++) {
+                if (arr[j] > arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+
+        System.out.println(Arrays.toString(arr));
+    }
+}
---
+++ javasepro/api-app3/src/com/alan/algorithm/Test2.java
@@ -0,0 +1,21 @@
+package com.alan.algorithm;
+
+import java.util.Arrays;
+
+// 目标：掌握选择排序的编写
+ public class Test2 {
+    public static void main(String[] args) {
+        //1. 准备一个数组
+        int[] arr = {5, 2, 3, 1, 4, 6};
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] > arr[j]) {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+}
