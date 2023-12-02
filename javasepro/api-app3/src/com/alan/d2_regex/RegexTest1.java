26a7cd7858eee3df2fad758ff89462f288904e31
LanLan77 <775172380@qq.com>
2023-12-02 23:04:10 +1030

    正则表达式

---
+++ javasepro/api-app3/src/com/alan/d2_regex/RegexTest1.java
@@ -0,0 +1,12 @@
+package com.alan.d2_regex;
+// 目标：使用正则表达式来检验数据格式的合法性
+// 需求：检验QQ号码是否正确，要求全部是数字，长度是6-20之间，不能用0开头
+public class RegexTest1 {
+    public static void main(String[] args) {
+
+    }
+
+    public static boolean checkQQ(int arr){
+        return false;
+    }
+}
