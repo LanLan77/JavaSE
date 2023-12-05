cd3082f2a23dd82110c13262bf198e1e45056a3f
LanLan77 <775172380@qq.com>
2023-12-05 23:30:46 +1030

    正则表达式：书写规则

--- javasepro/api-app3/src/com/alan/d2_regex/RegexTest3.java
+++ javasepro/api-app3/src/com/alan/d2_regex/RegexTest3.java
@@ -1,4 +1,21 @@
 package com.alan.d2_regex;
 
+import java.util.Scanner;
+
+// 目标：校验用户输入的电话，邮箱，时间是否合法
 public class RegexTest3 {
+    public static void main(String[] args) {
+
+    }
+
+    public static void checkPhone(){
+        System.out.println("请您输入您的电话号码(座机或手机)：");
+        Scanner scanner = new Scanner(System.in);
+        String phone = scanner.nextLine();
+
+        if(phone.matches("正则表达式")){
+            
+
+        }
+    }
 }
